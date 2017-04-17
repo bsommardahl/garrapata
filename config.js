0dcf01548dfc6d76fa1b5d291fe4fa69f130509e
Byron Sommardahl <byron@acklenavenue.com>
2017-04-16 18:54:57 -0600

    Working like  champ.

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 module.exports =
   {
-    "cronTime": "00 30 11 * * 1-5",
-    "timeZone": "America/Chicago",
+    "url": "https://sommardahl-status.herokuapp.com/",
+    "interval": 10000,
     "targets": [
         { "name": "Byron's Firewall", "address": "192.168.1.1" },
+        { "name": "Byron's Wifi Router", "address": "10.0.1.1" },
         { "name": "Olwing's Router", "address": "192.168.1.50" },
         { "name": "Tegucigalpa", "address": "192.168.50.1" },
+        { "name": "Unlocator DNS 1", "address": "185.37.37.37" },
+        { "name": "Unlocator DNS 2", "address": "185.37.39.39" },
         { "name": "google.com", "address": "google.com"}
     ]
 };
--- server.js
+++ server.js
@@ -3,22 +3,30 @@ var config = require('./config.js');
 var pinger = require('./pinger.js');
 var getMac = require('getmac');
 
-getMac.getMac(function(err,macAddress){
-    if (err)  throw err
-    console.log("MAC Address", macAddress)
-    pinger.ping(config.targets)
-    .then((t) => {
-        var payload = { source: macAddress, time: new Date(), config: config, targets: t};
-        console.log(payload);
-    });
-})
+var request = require('request');
 
-// var job = new CronJob({
-//   cronTime: config.cronTime,
-//   onTick: function() {
-//     pinger.ping();
-//   },
-//   start: false,
-//   timeZone: config.timeZone
-// });
-// job.start();
+var log = () => {
+    getMac.getMac(function(err, macAddress){
+        if (err)  throw err
+        pinger.ping(config.targets)
+        .then((t) => {
+            var payload = { source: macAddress, time: new Date(), targets: t};
+            request({
+                method: 'POST',
+                json: true,
+                url: config.url,
+                body: payload
+            }, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    console.log(`logged at ${new Date}`);
+                }
+            });
+        });
+    });
+};
+
+console.log(`Running every ${config.interval} milliseconds...`);
+log();
+setInterval(() => {
+    log();
+}, config.interval);
