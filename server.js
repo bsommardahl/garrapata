4b6d268297e9ba2d9a259e09868e705cde03fe45
Byron Sommardahl <byron@acklenavenue.com>
2017-04-17 16:27:43 -0600

    Changing to hostname

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 var CronJob = require('cron').CronJob;
 var config = require('./config.js');
 var pinger = require('./pinger.js');
-var getMac = require('getmac');
+var os = require("os");
 
 var request = require('request');
 
 var log = () => {
-    getMac.getMac(function(err, macAddress){
-        if (err)  throw err
-        pinger.ping(config.targets)
-        .then((t) => {
-            var payload = { source: macAddress, time: new Date(), targets: t};
-            request({
-                method: 'POST',
-                json: true,
-                url: config.url,
-                body: payload
-            }, (error, response, body) => {
-                if (!error && response.statusCode == 200) {
-                    console.log(`logged at ${new Date}`);
-                }
-            });
+    pinger.ping(config.targets)
+    .then((t) => {
+        var payload = { source: os.hostname(), time: new Date(), targets: t};
+        request({
+            method: 'POST',
+            json: true,
+            url: config.url,
+            body: payload
+        }, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                console.log(`logged at ${new Date}`);
+            }
         });
     });
 };
